329cd6228a6124f961ed47ff79f2b2a2b09185c8
ocarton <git@ocarton.fr>
2015-10-10 15:41:15 +0200

    Login fonctionne

--- app.js
+++ app.js
@@ -54,10 +54,12 @@ angular.module( 'sample', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$routeChangeSuccess', function(e, nextRoute){
     if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
-      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample' ;
+      $scope.pageTitle = nextRoute.$$route.pageTitle;
     }
   });
 })
+
+
 
 ;
 
