7408d37126a0c40201f1905cb7f8084895a5be04
ocarton <git@ocarton.fr>
2015-10-10 12:15:24 +0200

    Signin fonctionne mais bandeau trop fin

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-angular.module('YOUR-APP-NAME', ['auth0', 'angular-storage', 'angular-jwt'])
-.config(function (authProvider) {
+angular.module( 'sample', [
+  'auth0',
+  'ngRoute',
+  'sample.home',
+  'sample.login',
+  'angular-storage',
+  'angular-jwt'
+])
+.config( function myAppConfig ( $routeProvider, authProvider, $httpProvider, $locationProvider,
+  jwtInterceptorProvider) {
+  $routeProvider
+    .when( '/', {
+      controller: 'HomeCtrl',
+      templateUrl: 'home/home.html',
+      pageTitle: 'Homepage',
+      requiresLogin: true
+    })
+    .when( '/login', {
+      controller: 'LoginCtrl',
+      templateUrl: 'login/login.html',
+      pageTitle: 'Login'
+    });
+
+
   authProvider.init({
-    domain: 'ocarton.eu.auth0.com',
-    clientID: 'DoSDUdFuDMBT70SDScOUL2ZrHGjL1Sex'
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    loginUrl: '/login'
+  });
+
+  jwtInterceptorProvider.tokenGetter = function(store) {
+    return store.get('token');
+  }
+
+  // Add a simple interceptor that will fetch all requests and add the jwt token to its authorization header.
+  // NOTE: in case you are calling APIs which expect a token signed with a different secret, you might
+  // want to check the delegation-token example
+  $httpProvider.interceptors.push('jwtInterceptor');
+}).run(function($rootScope, auth, store, jwtHelper, $location) {
+  $rootScope.$on('$locationChangeStart', function() {
+    if (!auth.isAuthenticated) {
+      var token = store.get('token');
+      if (token) {
+        if (!jwtHelper.isTokenExpired(token)) {
+          auth.authenticate(store.get('profile'), token);
+        } else {
+          $location.path('/login');
+        }
+      }
+    }
+
   });
 })
-.run(function(auth) {
-  // This hooks al auth events to check everything as soon as the app starts
-  auth.hookEvents();
-});
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$routeChangeSuccess', function(e, nextRoute){
+    if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
+      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample' ;
+    }
+  });
+})
 
+;
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
-});
