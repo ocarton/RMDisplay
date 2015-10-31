4ba0e699f279f7c7cb8439d63bb4a82dc654d0bd
ocarton <git@ocarton.fr>
2015-10-31 01:16:36 +0100

    Les barres se mettent à jour

--- charts/ARVEChart.js
+++ charts/ARVEChart.js
@@ -8,7 +8,7 @@ $scope.ARVEChart = function() {}
 
 var fList= ["data/arveS-2.json", "data/arveS-1.json", "data/arveS.json", "data/arveS1.json", "data/arveS2.json"];
 
-var margin = {top: 20, right: 100, bottom: 30, left: 45},
+var margin = {top: 20, right: 45, bottom: 30, left: 45},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -17,14 +17,17 @@ var x = d3.scale.ordinal()
 
 var y = d3.scale.linear()
     .rangeRound([height, 0]);
+    
+var curWeek = 2;
 
 var catList = ["unknown", "sickness", "idle", "ti", "training",  "presales", "billable", "vacation"];
 
 var color = d3.scale.ordinal()
     .range(["#00000","#FFD300", "#FF0000", "#999999", "#FF7400", "#00E400", "#4416D5", "#00C9C9"])
     .domain(catList);
+var barOpacity=0.7, sideBarsWidth=0.3;
 
-var transitionDuration = 1000;
+var t, transitionDuration = 1000;
 
 var xAxis = d3.svg.axis()
     .scale(x)    
@@ -39,7 +42,6 @@ var yAxis = d3.svg.axis()
 var xBars = [], xBarsS = [], xBarsP = [];
 
 var groupC, groupL, groupR; //Group of rectangles which compose one bar
-
 var root = [];
 
 function initNodeData(n) {
@@ -80,24 +82,21 @@ function initNodeData(n) {
   n.y0 = 0;
 }
 
-function drawChart(week, transition) {
-    var t = null;
-	  t = svg.transition().duration(transitionDuration);    
-    if(transition =="left") {
-      // Hiding side bars
-      groupL.transition().duration(transitionDuration).attr("transform", function(d) {return "translate(" + (x(d.name)) + ",0) scale(0, 1)"; });  
-      groupR.transition().duration(transitionDuration).attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) +  ",0) scale(0, 1)"; });
-    }
-    // Securing max values
-    if (week < 1) {week = 1};
-    if (week > root.length-2) {week = root.length-2};    
+function loadBarsData() {
+	  t = svg.transition().duration(transitionDuration);  
     // Building the bars list
-    angular.forEach(root[week].children, function(d) {xBars = xBars.concat(d);});
-    xBars = xBars.concat(root[week]);
-    angular.forEach(root[week+1].children, function(d) {xBarsS = xBarsS.concat(d);});
-    xBarsS = xBarsS.concat(root[week+1]);
-    angular.forEach(root[week-1].children, function(d) {xBarsP = xBarsP.concat(d);});
-    xBarsP = xBarsP.concat(root[week-1]);    
+    console.log("Week index="+curWeek);
+    xBars = [];
+    angular.forEach(root[curWeek].children, function(d) {xBars = xBars.concat(d);});
+    xBars = xBars.concat(root[curWeek]); 
+
+    xBarsS = [];
+    angular.forEach(root[curWeek+1].children, function(d) {xBarsS = xBarsS.concat(d);});
+    xBarsS = xBarsS.concat(root[curWeek+1]);
+    
+    xBarsP = [];    
+    angular.forEach(root[curWeek-1].children, function(d) {xBarsP = xBarsP.concat(d);});
+    xBarsP = xBarsP.concat(root[curWeek-1]); console.log(xBarsP); 
     
     // Calculating minimal vacation percentage  
     var minVacSh0= d3.min(xBars.map(function(d) {return -d.vacSh;}));
@@ -105,7 +104,7 @@ function drawChart(week, transition) {
     var minVacSh2 = d3.min(xBarsP.map(function(d) {return -d.vacSh;}));
     var minVacSh = d3.min([minVacSh1, minVacSh0, minVacSh2]);        
     // Setting a scale from y values
-    y.domain([minVacSh, 1]);     
+    y.domain([minVacSh, 1]);   
     // Displaying yAxis
     if (!yAxisGroup) {
       yAxisGroup = svg.append("g")
@@ -122,9 +121,238 @@ function drawChart(week, transition) {
       t.select(".y Axis").call(yAxis);
       t.select("line.y")
     }    
-    
+     
     // Setting a scale with all x labels    
     x.domain(xBars.map(function(d) { return d.name; }));
+    // Building the bars lists
+
+   //Building or rebuilding the bars
+   if (!groupC) {
+      groupC = svg.selectAll(".barCenter")
+        .data(xBars)
+        .enter().append("g")
+        .attr("class", "barCenter")
+        .attr("opacity", 0);   
+      angular.forEach(catList, function(c){
+          groupC.append("rect")
+            .attr("width", x.rangeBand()) //Bar width
+            .attr("class", c)
+            .attr("y", function(d) {return d.y0;}) //Bar position
+            .attr("height", function(d) {d.y0 += y(1-d[c]/d.tot); return y(1-d[c]/d.tot)}) //Bar length
+            .style("fill", color(c));       
+      });    
+   }
+   else {
+      angular.forEach(xBars, function(d){d.y0=0;});  
+      
+      groupL = svg.selectAll(".barCenter")
+        .data(xBars);
+      groupL
+        .enter().append("g")
+        .attr("class", "barCenter");
+
+      groupL.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});  
+      angular.forEach(catList, function(c){
+          groupL.each(function(parDat) {
+            rect = d3.select(this).selectAll("rect."+c);
+            rect
+                  .attr("width", x.rangeBand()) //Bar width        
+                  .attr("y", function(d) {
+                    return parDat.y0;}) //Bar position
+                  .attr("height", function(d) {
+                      parDat.y0 += y(1-parDat[c]/parDat.tot);    
+                      return y(1-parDat[c]/parDat.tot)}); 
+          });
+      });     
+   }
+   
+   if(!groupL) {
+      groupL = svg.selectAll(".barLeft")
+        .data(xBarsP)
+        .enter().append("g")
+        .attr("class", "barLeft")
+        .attr("transform", function(d) {return "translate(" + x(d.name)+ ",0) scale(0, 1)"; });  // Position of next bar
+      angular.forEach(catList, function(c){
+          groupL.append("rect")
+            .attr("width", x.rangeBand()*sideBarsWidth) //Bar width
+            .attr("class", c)
+            .attr("opacity", barOpacity)        
+            .attr("y", function(d) {return d.y0;}) //Bar position
+            .attr("height", function(d) {//console.log(c+":"+d[c]);
+                d.y0 += y(1-d[c]/d.tot);      
+                return y(1-d[c]/d.tot)}) //Bar length
+            .style("fill", color(c));         
+      });       
+   }
+   else {
+
+    angular.forEach(xBarsP, function(d){d.y0=0;});
+    
+    groupL = svg.selectAll(".barLeft")
+      .data(xBarsP);
+    groupL
+      .enter().append("g")
+      .attr("class", "barLeft");
+      
+    groupL.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});  
+    angular.forEach(catList, function(c){
+        groupL.each(function(parDat) {
+          rect = d3.select(this).selectAll("rect."+c);
+          rect
+                .attr("width", x.rangeBand()*sideBarsWidth) //Bar width        
+                .attr("y", function(d) {
+                  return parDat.y0;}) //Bar position
+                .attr("height", function(d) {
+                    parDat.y0 += y(1-parDat[c]/parDat.tot);    
+                    return y(1-parDat[c]/parDat.tot)}); 
+        });
+    });
+   }
+   var legendY1 = [], legendY2 = [];   
+   var lastLegendPos = -5;
+   var legPos, lastCat = ""; 
+   if(!groupR) {
+      groupR = svg.selectAll(".barRight")
+          .data(xBarsS)
+          .enter().append("g")
+          .attr("class", "barRight")
+          .attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) + ",0) scale(0,1) "; })  // Position of next bar
+          .on ("click", click);
+           
+      angular.forEach(catList, function(c){
+          groupR.append("rect")
+            .attr("width", x.rangeBand()*sideBarsWidth) //Bar width
+            .attr("class", c)
+            .attr("opacity", barOpacity)          
+            .attr("y", function(d) {return d.y0;}) //Bar position
+            .attr("height", function(d) {
+                legPos = d.y0 + y(1-d[c]/d.tot)/2;              
+                legendY2[c] = d3.max([15+lastLegendPos, legPos]); 
+                legendY1[c] = legPos;         
+                if (c != lastCat) {
+                  if (lastCat != "") {lastLegendPos = legendY2[lastCat];}
+                  lastCat=c; };
+                  d.y0 += y(1-d[c]/d.tot);
+                  return y(1-d[c]/d.tot)
+                })
+            .style("fill", color(c));       
+      });           
+   }
+   else{
+     
+    angular.forEach(xBarsS, function(d){d.y0=0;});
+    
+    groupR = svg.selectAll(".barRight")
+      .data(xBarsS);
+    groupR
+      .enter().append("g")
+      .attr("class", "barRight");
+     
+      angular.forEach(catList, function(c){
+        groupR.each(function(parDat) {
+          rect = d3.select(this).selectAll("rect."+c);
+          rect        
+            .attr("y", function(d) {return parDat.y0;}) //Bar position
+            .attr("height", function(d) {
+                legPos = parDat.y0 + y(1-parDat[c]/parDat.tot)/2;              
+                legendY2[c] = d3.max([15+lastLegendPos, legPos]); 
+                legendY1[c] = legPos;         
+                if (c != lastCat) {
+                  if (lastCat != "") {lastLegendPos = legendY2[lastCat];}
+                  lastCat=c;
+                  }
+                parDat.y0 += y(1-parDat[c]/parDat.tot);
+                return y(1-parDat[c]/parDat.tot)
+                
+         })
+      });       
+   });
+   }
+ 
+   // Positionning legend
+   var legend = svg.select(".barRight:last-child").selectAll(".legend")
+        .data(catList)
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("opacity", 1)        
+        .attr("transform", function(d) {return "translate(" + (x.rangeBand()*sideBarsWidth+20) + "," + legendY2[d] + ")"; });
+  
+   // Setting line
+   legend.append("line")
+        .attr("stroke-width", 2)
+        .attr("stroke", "black")
+        .attr("opacity", 1)
+        .attr("x1", -20).attr("x2", 10)
+        .attr("y1", function(d) {return (legendY1[d]-legendY2[d]);}).attr("y2", 0);        
+
+  
+   // Setting text    
+   legend.append("text")
+        .attr("x", 13)
+        .attr("dy", ".35em")
+        .attr("opacity",1)        
+        .text(function(d) {return d;});
+    
+}
+
+function slideChart(transition) {
+  
+  if(transition =="left") {
+    curWeek = curWeek+1;
+    // Securing max values
+    if (curWeek > root.length-2) {curWeek = root.length-2};        
+    // Moving bars
+ /*   groupL.selectAll("rect")
+      .attr("transform", function(d) {return "translate(" + x.rangeBand()/2 + ",0)  scale(2, 1)"; });    
+    groupC.selectAll("rect")
+      .attr("transform", function(d) {return "translate(" + (x.rangeBand()) + ",0) scale(0.5, 1)"; });
+    groupR.selectAll("rect")
+      .attr("transform", function(d) {return "translate(" + (x.rangeBand()) + ",0) scale(0, 1)"; }); 
+ */ loadBarsData();
+    
+    // Redrawing xAxis
+    svg.select("g").selectAll("line.x").data("xAxis").enter().append("line")
+      .attr("class", "x")
+      .attr("x1", 0).attr("x2", width)
+      .attr("y1", y(0)).attr("y2", y(0))
+      .style("stroke", "#000");         
+    // Building xAxis
+     if (!xAxisGroup) {
+      xAxisGroup = svg.append("g")
+        .attr("class", "x axis")
+        .call(xAxis);
+    }
+    else {
+      t.select(".x Axis").call(xAxis);
+    }       
+    
+    
+  }
+  else if(transition =="right") {
+    curWeek = curWeek -1;
+    if (curWeek < 1) {curWeek = 1};
+    // Moving bars
+    groupL.transition().duration(transitionDuration)
+      .attr("transform", function(d) {return "translate(" + (x(d.name)-x.rangeBand()*sideBarsWidth) + ",0) scale(0, 1)"; });
+    groupC.transition().duration(transitionDuration)
+      .attr("transform", function(d) {return "translate(" + (x(d.name)-x.rangeBand()*sideBarsWidth) + ",0) scale("+sideBarsWidth+", 1)"; })
+      .attr("opacity", barOpacity);
+    groupR.transition().duration(transitionDuration)
+      .attr("transform", function(d) {return "translate(" + (x(d.name))+x.rangeBand() +  ",0) scale(0, 1)"; })
+      .attr("opacity", 1);
+  }   
+}
+
+
+
+var drawChart = function () {
+	  t = svg.transition().duration(transitionDuration);  
+     
+    loadBarsData();
+
+    groupC.attr("transform", function(d) {return "translate(" + x(d.name) + ",0)"; });  // Position of next bar
+    groupL.attr("transform", function(d) {return "translate(" + x(d.name)+ ",0) scale(0, 1)"; });  // Position of next bar
+    groupR.attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) + ",0) scale(0,1) "; });  // Position of next bar
  
     // Redrawing xAxis
     svg.select("g").selectAll("line.x").data("xAxis").enter().append("line")
@@ -140,97 +368,18 @@ function drawChart(week, transition) {
     }
     else {
       t.select(".x Axis").call(xAxis);
-    } 
-
-    // Building the bars lists
-    groupC = svg.selectAll(".barCenter")
-      .data(xBars)
-      .enter().append("g")
-      .attr("class", "barCenter")
-      .attr("opacity", 0)
-      .attr("transform", function(d) {return "translate(" + x(d.name) + ",0)"; });  // Position of next bar
-    groupL = svg.selectAll(".barLeft")
-      .data(xBarsP)
-      .enter().append("g")
-      .attr("class", "barLeft")
-      .attr("transform", function(d) {return "translate(" + x(d.name)+ ",0) scale(0, 1)"; });  // Position of next bar
-    groupR = svg.selectAll(".barRight")
-      .data(xBarsS)
-      .enter().append("g")
-      .attr("class", "barRight")
-      .attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) + ",0) scale(0,1) "; });  // Position of next bar
-
-   //Building the bars
-   angular.forEach(catList, function(c){
-      groupC.append("rect")
-        .attr("width", x.rangeBand()) //Bar width
-        .attr("class", c)
-        .attr("y", function(d) {return d.y0;}) //Bar position
-        .attr("height", function(d) {d.y0 += y(1-d[c]/d.tot); return y(1-d[c]/d.tot)}) //Bar length
-        .style("fill", color(c));       
-   });
-   var legendY1 = [], legendY2 = [];   
-   var lastLegendPos = -5;
-   var legPos, lastCat = ""; 
-   angular.forEach(catList, function(c){
-      groupR.append("rect")
-        .attr("width", x.rangeBand()/2) //Bar width
-        .attr("class", c)
-        .attr("opacity", 0.4)          
-        .attr("y", function(d) {return d.y0;}) //Bar position
-        .attr("height", function(d) {
-            legPos = d.y0 + y(1-d[c]/d.tot)/2;              
-            legendY2[c] = d3.max([10+lastLegendPos, legPos]); 
-            legendY1[c] = legPos;         
-            if (c != lastCat) {
-              if (lastCat != "") {lastLegendPos = legendY2[lastCat];}
-              lastCat=c; };
-          console.log("c="+c+" lastLegendPos="+lastLegendPos+" y="+ (d.y0 + y(1-d[c]/d.tot)/2)+" max="+legendY2[c]);
-            d.y0 += y(1-d[c]/d.tot);
-            return y(1-d[c]/d.tot)})
-        .style("fill", color(c));       
-   });   
-   angular.forEach(catList, function(c){
-      groupL.append("rect")
-        .attr("width", x.rangeBand()/2) //Bar width
-        .attr("class", c)
-        .attr("opacity", 0.4)        
-        .attr("y", function(d) {return d.y0;}) //Bar position
-        .attr("height", function(d) {
-            d.y0 += y(1-d[c]/d.tot);
-            return y(1-d[c]/d.tot)}) //Bar length
-        .style("fill", color(c));  
-   });   
-   // Positionning legend
-   var legend = svg.select(".barRight:last-child").selectAll(".legend")
-        .data(catList)
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("opacity", 1)        
-        .attr("transform", function(d) {return "translate(" + (x.rangeBand()/2) + "," + legendY2[d] + ")"; });
-  
-   // Setting line
-   legend.append("line")
-        .attr("stroke-width", 2)
-        .attr("stroke", "black")
-        .attr("opacity", 1)
-        .attr("x1", 0).attr("x2", 10)
-        .attr("y1", function(d) {return (legendY1[d]-legendY2[d]);}).attr("y2", 0);        
-
-  
-   // Setting text    
-   legend.append("text")
-        .attr("x", 13)
-        .attr("dy", ".35em")
-        .attr("opacity",1)        
-        .text(function(d) {return d;});
+    }      
         
-
    groupC.transition().duration(transitionDuration).attr("opacity", 1).each("end", function(e, i){
-      groupL.transition().duration(transitionDuration).attr("transform", function(d) {return "translate(" + (x(d.name)-x.rangeBand()/2) + ",0) scale(1, 1)"; });  // Position of next bar
+      groupL.transition().duration(transitionDuration).attr("transform", function(d) {return "translate(" + (x(d.name)-x.rangeBand()*sideBarsWidth) + ",0) scale(1, 1)"; });  // Position of next bar
       groupR.transition().duration(transitionDuration).attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) +  ",0) scale(1, 1)"; });
    });   
+   
 }
+
+function click(d) {
+  slideChart("left");    
+};
 
 var svg = d3.select("#ARVEChart").append("svg")
     .attr("class", "chart")
@@ -260,7 +409,7 @@ d3.json(fList[0], function(error, data0) {
           root[4] = data4;    
           initNodeData(root[4]);
   
-          drawChart(2);
+          drawChart();       
           });      
         });      
       });      
