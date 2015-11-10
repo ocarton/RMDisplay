feb82c1e7cf0dff9043906e0ce4e2c79e34f4a35
ocarton <git@ocarton.fr>
2015-11-10 22:49:45 +0100

    Compensation des unknowns

--- charts/ARVEChart.js
+++ charts/ARVEChart.js
@@ -4,13 +4,25 @@ angular.module('ARVEChart', [])
 
 .controller( 'ARVECtrl', function ARVEController( $scope, auth, $http, $location, store) {
 
+$scope.checkVac = {
+  value : false
+}
+
+$scope.checkUnknown = {
+  value : false
+}
+
+$scope.displayCat = function () {
+  loadBarsData();  
+}
+
 $scope.ARVEChart = function() {}
 
 var fList= ["data/arveP2.json", "data/arveP1.json", "data/arveS.json", "data/arveS1.json", "data/arveS2.json"];
 
 var margin = {top: 40, right: 45, bottom: 30, left: 55},
     width = 1000 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 800 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width*.9], .5 ,0.4); //,1 : Space between bars
@@ -35,7 +47,7 @@ var color = d3.scale.ordinal()
     .domain(catList);
 var barOpacity=0.7, sideBarsWidth=0.3;
 
-var transitionDuration = 1000;
+var t, transitionDuration = 1000;
 
 var root = [], activeGroup = [], newGroupArray = [];
 var xBars = [], xBarsS = [], xBarsP = [];
@@ -50,11 +62,15 @@ var svg = d3.select("#ARVEChart").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+function semText(week) {
+  if (week==2){return "S"}
+  else if (week<2){return "S"+(week-2)}
+  else {return "S+"+(week-2)}
+}
+
 // defining graph title
 function graphTitle () {
-  if (curWeek == 2) { return "ARVE WTR Semaine S"}
-  else if (curWeek >2) {return "ARVE WTR Semaine S+"+(curWeek-2)}
-  else {return "ARVE WTR Semaine S-"+(2-curWeek)}
+  return "ARVE WTR Semaine "+semText(curWeek)+"&nbsp;&nbsp;&nbsp;("+semText(curWeek-1)+" à g., "+semText(curWeek+1)+" à dr.)"
 }
 
 // function for the y grid lines
@@ -108,7 +124,8 @@ function loadBarsData() {
     title
       .html(graphTitle());
   
-	  var t = svg.transition().duration(transitionDuration);  
+    t = svg.transition().duration(transitionDuration);  
+  
     // Building the bars list
     console.log("Week index="+curWeek);
     xBars = [];
@@ -125,13 +142,27 @@ function loadBarsData() {
     angular.forEach(activeGroup[curWeek-1].children, function(d) {xBarsP = xBarsP.concat(d);});
     xBarsP = xBarsP.concat(activeGroup[curWeek-1]);
     angular.forEach(xBarsP, function(d){d.y0=0;});     
-    
+
     // Calculating minimal vacation percentage  
     var minVacSh0= d3.min(xBars.map(function(d) {return -d.vacSh;}));
     var minVacSh1 = d3.min(xBarsS.map(function(d) {return -d.vacSh;}));
     var minVacSh2 = d3.min(xBarsP.map(function(d) {return -d.vacSh;}));
-    var minVacSh = d3.min([minVacSh1, minVacSh0, minVacSh2]);        
-  
+    var minVacSh = d3.min([minVacSh1, minVacSh0, minVacSh2]);    
+
+    var displayList = catList.slice();    
+    // Removing vacations if box is unchecked 
+    if ($scope.checkVac.value == false) {
+      minVacSh = 0
+      var posVac = displayList.indexOf("vacation");
+      if (posVac > -1) {displayList.splice(posVac, 1)}
+    }      
+
+    //Removing unknowns if checkbox is checked
+    if ($scope.checkUnknown.value != false) {
+      var posUnknown = displayList.indexOf("unknown");
+      if (posUnknown > -1) {displayList.splice(posUnknown, 1)}
+    }       
+
       // Setting a scale from x and y values
       y.domain([minVacSh, 1]);
       x.domain(xBars.map(function(d) {return d.name; }))
@@ -142,7 +173,7 @@ function loadBarsData() {
       var t1 = t.transition();
       t.selectAll(".y.axis").call(yAxis);    
       t.selectAll(".y.grid").call(make_y_axis());         
-      svg.selectAll(".y.grid").selectAll("line").filter(function(d){return d == 0;})
+      svg.selectAll(".y.grid").selectAll("line").filter(function(d){return (Math.round(d*10) == d*10);})
       .style("stroke", "#000");                    
       
       // Setting a scale with all x labels    
@@ -162,54 +193,64 @@ function loadBarsData() {
       
       groupC.on("click", drillDown); 
       svg.select("#"+xBars[xBars.length-1].name+".barCenter").on("click", drillUp);         
-  
+
       t.selectAll(".barCenter")
         .attr("opacity", 1)       
         .attr("transform", function(d) {return "translate(" + x(d.name)+ ",0) scale(1, 1)"; })  // Position of next bar
-      
+
       groupC.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});  
-  
+
       rect = groupC.selectAll("rect")
-        .data(catList);
+        .data(displayList, function(d){return(d)});
       rect
         .enter().append("rect")
+        .attr("opacity", 0)          
         .attr("class", function(d) {return d;})
         .style("fill", function(d) {return color(d);});
+      rect.exit().remove();
 
       svg.selectAll(".barCenter").selectAll(".legend").remove();
 
-      angular.forEach(catList, function(c){
+      groupC.each(function(d) {
+        d.catSum = d.tot;         
+        if ($scope.checkUnknown.value != false) {
+          d.catSum = d.catSum-d.unknown;
+        } 
+      });
+
+      angular.forEach(displayList, function(c){
           groupC.each(function(parDat) {
             svg.selectAll("#"+parDat.name+".barCenter").selectAll("rect."+c)
+                  .attr("opacity", 1)              
                   .attr("width", x.rangeBand()) //Bar width        
                   .attr("y", function(d) {return parDat.y0;}) //Bar position
                   .attr("height", function(d) {
-                      if (c=="billable") {parDat.midBar = parDat.y0+y(1-parDat[c]/parDat.tot/2)}
-                      parDat.y0 += y(1-parDat[c]/parDat.tot);
-                      return y(1-parDat[c]/parDat.tot)});
+                      if (c=="billable") {parDat.midBar = parDat.y0+y(1-parDat[c]/parDat.catSum/2)}
+                      parDat.y0 += y(1-parDat[c]/parDat.catSum);
+                      return y(1-parDat[c]/parDat.catSum)});
             if (c == "billable") {
               var value = svg.selectAll("#"+parDat.name+".barCenter")
                 .append("g")
                 .attr("class", "legend")
-                 .attr("opacity",1)                   
-                .attr("transform", function(d) {return "translate(-10," + parDat.midBar + ")"; })
+                .attr("opacity",1)                   
+                .attr("transform", function(d) {return "translate(0," + parDat.midBar + ")"; })                
               //adding the white 'shadow' behind the text
               value
                 .append("text")
-                  .attr("x", 13)
+                  .attr("x", 3)
                   .attr("dy", ".35em")
                   .attr("font-size", "1.4em")                  
                   .attr("opacity",1)   
                   .attr("class", "shadow")                         
-                  .text(function(d){return d3.format(".2%")(parDat[c]/parDat.tot)});
+                  .text(function(d){return d3.format(".2%")(parDat[c]/parDat.catSum)});
               //adding the text
               value
                 .append("text")
-                  .attr("x", 13)
+                  .attr("x", 3)
                   .attr("dy", ".35em")
                   .attr("font-size", "1.4em")                   
                   .attr("opacity",1)   
-                  .text(function(d){return d3.format(".2%")(parDat[c]/parDat.tot)});              
+                  .text(function(d){return d3.format(".2%")(parDat[c]/parDat.catSum)});              
                                 
             }      
           });
@@ -231,30 +272,36 @@ function loadBarsData() {
         .attr("transform", function(d) {return "translate(" + (x(d.name)-x.rangeBand()*sideBarsWidth) + ",0) scale(1, 1)"; });  // Position of next bar      
       
       groupL.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});  
+      
+      groupL.each(function(d) {
+        d.catSum = d.tot;         
+        if ($scope.checkUnknown.value != false) {d.catSum = d.catSum-d.unknown;}
+      });      
   
       rect = groupL.selectAll("rect")
-        .data(catList);
+        .data(displayList, function(d){return(d)});
       rect
         .enter().append("rect")
         .attr("opacity", barOpacity)      
         .attr("class", function(d) {return d;})
         .style("fill", function(d) {return color(d);});
+      rect.exit().remove();
   
-      angular.forEach(catList, function(c){
+      angular.forEach(displayList, function(c){
           groupL.each(function(parDat) {
-            svg.selectAll("#"+parDat.name+".barLeft").selectAll("rect."+c)//.transition().duration(transitionDuration)
+            svg.selectAll("#"+parDat.name+".barLeft").selectAll("rect."+c)
                   .attr("width", x.rangeBand()*sideBarsWidth) //Bar width 
                   .attr("y", function(d) {return parDat.y0;}) //Bar position
                   .attr("height", function(d) {
-                      parDat.y0 += y(1-parDat[c]/parDat.tot);    
-                      return y(1-parDat[c]/parDat.tot)}); 
+                      parDat.y0 += y(1-parDat[c]/parDat.catSum);    
+                      return y(1-parDat[c]/parDat.catSum)}); 
           });
       });
   
       var legendY1 = [], legendY2 = [];   
       var lastLegendPos = -5;
       var legPos, lastCat = ""; 
-  
+
       groupR = svg.selectAll(".barRight")
         .data(xBarsS, function(d){return d.name});
       groupR
@@ -270,44 +317,50 @@ function loadBarsData() {
         .attr("opacity", 1)       
         .attr("transform", function(d) {return "translate(" + (x(d.name)+x.rangeBand()) +  ",0) scale(1, 1)"; }) // Position of next bar
       
-      groupR.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});  
+      groupR.selectAll("rect.unknown").attr("y", function(d){d.y0=0;});
+      
+      groupR.each(function(d) {
+        d.catSum = d.tot;         
+        if ($scope.checkUnknown.value != false) {d.catSum = d.catSum-d.unknown;}
+      });          
   
       rect = groupR.selectAll("rect")
-        .data(catList);
+        .data(displayList, function(d){return(d)});
       rect
         .enter().append("rect")
         .attr("opacity", barOpacity)      
         .attr("class", function(d) {return d;})
         .style("fill", function(d) {return color(d);});
+      rect.exit().remove();        
   
-      angular.forEach(catList, function(c){
+      angular.forEach(displayList, function(c){
           groupR.each(function(parDat) {
             svg.selectAll("#"+parDat.name+".barRight").selectAll("rect."+c)//.transition().duration(transitionDuration)
                   .attr("width", x.rangeBand()*sideBarsWidth) //Bar width 
                   .attr("y", function(d) {return parDat.y0;}) //Bar position
                   .attr("height", function(d) {
                   //legPos defines position of the legend based on position of the bar
-                  legPos = parDat.y0 + y(1-parDat[c]/parDat.tot)/2;              
+                  legPos = parDat.y0 + y(1-parDat[c]/parDat.catSum)/2;              
                   legendY2[c] = d3.max([20+lastLegendPos, legPos]); 
                   legendY1[c] = legPos;        
                   if (c != lastCat) {
                     if (lastCat != "") {lastLegendPos = legendY2[lastCat];}
                     lastCat=c;
                     }
-                  parDat.y0 += y(1-parDat[c]/parDat.tot);
-                  return y(1-parDat[c]/parDat.tot)}); 
+                  parDat.y0 += y(1-parDat[c]/parDat.catSum);
+                  return y(1-parDat[c]/parDat.catSum)}); 
           });
       });
   
       // Positionning legend
       legend = svg.select("#"+xBarsS[xBarsS.length-1].name+".barRight").selectAll(".legend")
-            .data(catList)
+            .data(displayList, function(d){return(d)})
       legend.exit().remove();
       var legEl = legend
             .enter().append("g")
             .attr("class", "legend")
             .attr("transform", function(d) {return "translate(" + (x.rangeBand()*sideBarsWidth+20) + "," + legendY2[d] + ")"; })
-            .attr("opacity", 0);
+            .attr("opacity", 0); 
       legEl
               .append("text")
                 .attr("x", 13)
@@ -322,11 +375,11 @@ function loadBarsData() {
               .attr("opacity", 1)
               .attr("x1", -20).attr("x2", 10)
               .attr("y1", function(d) {return (legendY1[d]-legendY2[d]);}).attr("y2", 0);        
-            
+              
       t.select("#"+xBarsS[xBarsS.length-1].name+".barRight").selectAll(".legend")
           .attr("opacity", 1)      
           .attr("transform", function(d) {return "translate(" + (x.rangeBand()*sideBarsWidth+20) + "," + legendY2[d] + ")"; });
-  
+
       // Setting line
       t.select("#"+xBarsS[xBarsS.length-1].name+".barRight").selectAll(".legend line")
             .attr("x1", -20).attr("x2", 10)
@@ -388,7 +441,8 @@ function slideChart(transition) {
       loadBarsData();        
       // Moving bars
       groupL.selectAll("rect")
-        .attr("transform", function(d) {return "translate("+ x.rangeBand()*sideBarsWidth +",0)  scale("+1/sideBarsWidth+", 1)"; });    
+        .attr("transform", function(d) {return "translate("+ x.rangeBand()*sideBarsWidth +",0)  scale("+1/sideBarsWidth+", 1)"; });
+      //t.selectAll(".barCenter").selectAll("rect")            
       groupC.selectAll("rect")
         .attr("transform", function(d) {return "translate(" + (x.rangeBand()) + ",0) scale("+sideBarsWidth+", 1)"; });
       groupR.selectAll("rect")
@@ -434,71 +488,82 @@ function drillUp(bar) {
     activeGroup = root;
     newGroupArray = [];    
     angular.forEach(root, function(d){
-      console.log("nouvelle barre : ");
       getNode(d, bar.name); 
     });
-    console.log(newGroupArray)
     activeGroup = newGroupArray;  
     loadBarsData();    
   }
 };
 
 function getNode (tree, name) {
-  console.log("entering search"+tree.name);
     if (tree.children) {
         tree.children.forEach(function (d) {
-          if (d.name ==name) {console.log("found");console.log(tree);newGroupArray = newGroupArray.concat(tree)}  
-          else {console.log("inspecting"+d.name);return getNode(d, name)}         
+          if (d.name ==name) {newGroupArray = newGroupArray.concat(tree)}  
+          else {return getNode(d, name)}         
         })
     }
-    else {  console.log("endnode found")
-      return null;
+    else {return null;
     }    
 }
 
 function drillDown(bar) {
   console.log("Drilling down "+bar.name);
   newGroupArray = [];
-  angular.forEach(activeGroup, function(d) {//console.log("nonfiltered");console.log(d);
+  angular.forEach(activeGroup, function(d) {
     newGroupArray = newGroupArray.concat(d.children.filter(function(d){return(d.name == bar.name)}));
   });
-  activeGroup = newGroupArray;  
-
-  loadBarsData();   
+  if (newGroupArray[0].children){
+    activeGroup = newGroupArray;  
+    loadBarsData();
+  }   
 };
 
 //-------------------------------------------------------------------------------
 // Main function. It loads the data
 //-------------------------------------------------------------------------------
-d3.json(fList[1], function(error, data1) {
-  if (error) throw error;  
-  root[1] = data1;    
-  initNodeData(root[1]);
-  d3.json(fList[2], function(error, data2) {
+function loadData() {
+    console.log("Reloading data")
+    d3.json(fList[1], function(error, data1) {
     if (error) throw error;  
-    root[2] = data2;    
-    initNodeData(root[2]);
-    d3.json(fList[3], function(error, data3) {
+    root[1] = data1;    
+    initNodeData(root[1]);
+    d3.json(fList[2], function(error, data2) {
       if (error) throw error;  
-      root[3] = data3;    
-      initNodeData(root[3]);
-      
-      drawChart();
-
-      // Those data are not required at the beginning,
-      // so they are loaded simultaneously as the drawing of the chart
-      // to save some time                
-      d3.json(fList[0], function(error, data0) {
+      root[2] = data2;    
+      initNodeData(root[2]);
+      d3.json(fList[3], function(error, data3) {
         if (error) throw error;  
-        root[0] = data0;
-        initNodeData(root[0]);
-      });
-      d3.json(fList[4], function(error, data4) {
-        if (error) throw error;  
-        root[4] = data4;    
-        initNodeData(root[4]);
+        root[3] = data3;    
+        initNodeData(root[3]);
+ 
+        drawChart();
+ 
+        // Those data are not required at the beginning,
+        // so they are loaded simultaneously as the drawing of the chart
+        // to save some time                
+        d3.json(fList[0], function(error, data0) {
+          if (error) throw error;  
+          root[0] = data0;
+          initNodeData(root[0]);
         });
+        d3.json(fList[4], function(error, data4) {
+          if (error) throw error;  
+          root[4] = data4;    
+          initNodeData(root[4]);
+          });
+        });      
       });      
-    });      
-  });      
+    })
+  }  
+
+/*function reloadGraph(){
+  svg.selectAll("g").remove();
+  drawChart();
+}*/
+
+//drawChart(function (d){return loadData();})  
+loadData();
+//drawChart();  
+//setInterval(loadData,100000); 
+  
 });
