110fea3385e4fbbab448255e2e70deaab23a5309
ocarton <git@ocarton.fr>
2016-05-06 19:06:21 +0200

    Navigation: corrected a 'double reload of page' problem.

--- charts/IdleBoard.js
+++ charts/IdleBoard.js
@@ -1,59 +1,81 @@
 /// <reference path="../typings/angular2/angular2.d.ts"/>
-var myApp = angular.module('IdleBoard', ['angularjs-dropdown-multiselect', 'dragularModule'])
-.controller('IdleCtrl', ['$scope', '$element', 'dragularService', function TodoCtrl($scope, $element, dragularService) {
+var myApp = angular.module('IdleBoard', ['ngCookies', 'angularjs-dropdown-multiselect', 'dragularModule'])
+.controller('IdleCtrl', ['$scope', '$cookies', '$element', 'dragularService', function ($scope, $cookes, $element, dragularService) {
     dragularService('.containerVertical', { removeOnSpill: false });
 
     var citiesList = [];
-    var skillList = ["FR03AA10_CSD_RTCP", "FR03AA12_MCS_OCSR", "FR03AA11_MCS_RTCB", "FR03AA06_CSD_DC", "FR03AA09_MCS_S&A", "FR03AA14_CSD_OCSP"];
+    var skillList = ["FR03AA05_BS_PLM" , "FR03AA02_BS_BTS", "FR03AA03_BS_M&SC", "FR03AA10_CSD_RTCP", "FR03AA12_MCS_OCSR", "FR03AA11_MCS_RTCB", "FR03AA06_CSD_DC", "FR03AA09_MCS_S&A", "FR03AA14_CSD_OCSP"];
     var color = d3.scale.ordinal()
-        .range(["#FFF4D4", "#FF9BB8", "#FFE69B", "#C6FF9B", "#ABA9FF", "#FFD4E0", "#FFFFFF"])
+        .range(["#FFE69B", "#FFF4D4", "#ABA9FF", "#FFF4D4", "#FF9BB8", "#FFE69B", "#C6FF9B", "#ABA9FF", "#FFD4E0", "#FFFFFF"])
         .domain(skillList);
     $scope.models = {
         selected: null,
         availableList: [],
     };
 
+    $scope.selBoxCityModel = [];
+    $scope.selBoxCityData = [];
+    $scope.selBoxSkillGModel = [];
+    $scope.selBoxSkillCModel = [];
     $scope.listSC = [
         {
-            id:1, value: 'FR03CS04 - ASD', label: 'ASD', list: [
-              { id: 1, value: 'FR03AA06_CSD_DC', label: 'DIGITAL', bgdcol: '#C6FF9B'},
-              { id: 2, value: 'FR03AA09_MCS_S&A', label: 'S&A', bgdcol: '#ABA9FF'},
-              { id: 3, value: 'FR03AA10_CSD_RTCP', label: 'RTC Pau', bgdcol: '#FFF4D4'},
-              { id: 4, value: 'FR03AA11_MCS_RTCB', label: 'RTC Bayonne', bgdcol: '#FFE69B'},
-              { id: 5, value: 'FR03AA12_MCS_OCSR', label: 'OCS Rennes', bgdcol: '#FF9BB8'},
-              { id: 6, value: 'FR03AA14_CSD_OCSP', label: 'OCS Paris', bgdcol: '#FFD4E0'}
+            id: 1, value: 'FR03CS04 - ASD', label: 'CSD ASD', list: [
+              { id: 1, value: 'FR03AA06_CSD_DC', label: 'DIGITAL', bgdcol: '#C6FF9B' },
+              { id: 2, value: 'FR03AA09_MCS_S&A', label: 'S&A', bgdcol: '#ABA9FF' },
+              { id: 3, value: 'FR03AA10_CSD_RTCP', label: 'RTC Pau', bgdcol: '#FFF4D4' },
+              { id: 4, value: 'FR03AA11_MCS_RTCB', label: 'RTC Bayonne', bgdcol: '#FFE69B' },
+              { id: 5, value: 'FR03AA12_MCS_OCSR', label: 'OCS Rennes', bgdcol: '#FF9BB8' },
+              { id: 6, value: 'FR03AA14_CSD_OCSP', label: 'OCS Paris', bgdcol: '#FFD4E0' }
             ]
         },
-        { id: 2, value: 'FR03CSD4 - C&IM', label: 'C&IM', list: [] },
-        { id: 3, value: 'FR03CS03 - COMM', label: 'COMM', list: [] },
-        { id: 4, value: 'FR03CS07 - ECM', label: 'ECM', list: [] },
-        { id: 5, value: 'FR03CS05 - EU&I', label: 'EU&I', list: [] },
-        { id: 6, value: 'FR03CS01 - MGT', label: 'MGT', list: [] },
-        { id: 7, value: 'FR03CS06 - PER', label: 'PER', list: [] },
-        { id: 8, value: 'FR03CS02 - TMP', label: 'TMP', list: [] }
+        { id: 2, value: 'FR03CSD4 - C&IM', label: 'CSD C&IM', list: [] },
+        { id: 3, value: 'FR03CS03 - COMM', label: 'CSD COMM', list: [] },
+        { id: 4, value: 'FR03CS07 - ECM', label: 'CSD ECM', list: [] },
+        { id: 5, value: 'FR03CS05 - EU&I', label: 'CSD EU&I', list: [] },
+        { id: 6, value: 'FR03CS01 - MGT', label: 'CSD MGT', list: [] },
+        { id: 7, value: 'FR03CS06 - PER', label: 'CSD PER', list: [] },
+        { id: 8, value: 'FR03CS02 - TMP', label: 'CSD TMP', list: [] },
+        { id: 9, value: 'FR03AAL01-SUD', label: 'ADM SUD', list: [] },
+        { id: 10, value: 'FR03AAL02-NORD', label: 'ADM NORD', list: [] },
+        { id: 11, value: 'FR03AAL03-OUEST', label: 'ADM OUEST', list: [] },
+        { id: 12, value: 'FR03AAL04-NExT', label: 'ADM NextT', list: [] },
+        { id: 13, value: 'FR03AAL05-HERMES', label: 'ADM HERM', list: [] },
+        { id: 14, value: 'FR03PB03 - BS', label: 'PBS BS', list: [] }
     ];
 
-
-    //Forcing selection of ASD
-    $scope.selBoxSkillCModel = $scope.listSC.slice(0,1);
+    //------------Initialization of the dropdown lists
     $scope.selBoxSkillCData = $scope.listSC;
+    //if ($cookies.get('SkillC') == "") { 
+    $scope.selBoxSkillCModel = $scope.listSC.slice(0, 1);
+    //}
+    //else { $scope.selBoxSkillCModel = $cookies.get('SkillC');}
     $scope.selBoxSkillCSettings = {
         smartButtonMaxItems: 3,
         externalIdProp: ''
     };
 
-    $scope.selBoxSkillGModel = $scope.listSC[0].list.slice(0);
     $scope.selBoxSkillGData = $scope.listSC[0].list;
+    $scope.selBoxSkillGModel = $scope.listSC[0].list.slice(0);
     $scope.selBoxSkillGSettings = {
         smartButtonMaxItems: 0,
         externalIdProp: ''
     };
 
     $scope.selBoxCitySettings = {
-        smartButtonMaxItems: 1,
+        smartButtonMaxItems: 3,
         externalIdProp: ''
     };
 
+    $scope.storeSelected = {
+        onItemSelect: function (item) {
+            console.log(item);
+        },
+        onItemDeselect: function (item) {
+            console.log(item);
+        }
+    };
+
+    //------------Filters for the lists of each column
     $scope.col1Filter = function (item) {
         var itemSelected = false;
         if (item["Global practice"] == "AMOA"
@@ -93,6 +115,8 @@ var myApp = angular.module('IdleBoard', ['angularjs-dropdown-multiselect', 'drag
         return itemSelected;
     }
 
+
+    //------------Data loading of the idle people
     var dsv = d3.dsv(";", "text/plain; charset=ISO-8859-1");
 
     dsv("data/dispos.csv", function (error, data) {
@@ -106,38 +130,25 @@ var myApp = angular.module('IdleBoard', ['angularjs-dropdown-multiselect', 'drag
         $scope.models.availableList[7] = { "items": [] };
         $scope.models.availableList[8] = { "items": [] };
         $scope.models.availableList[9] = { "items": [] };
+        var cityIndex = 1;
         data.forEach(function (d) {
             d.color = color(d["Prod  Unit Label"]);
             //Creation of citiesList content from data available in dispos.csv
             if (citiesList.map(function (e) { return e.value; }).indexOf(d["Office Base"]) == -1) {
                 var newCity = {
-                    id: citiesList.length + 1,
+                    id: cityIndex,
                     value: d["Office Base"],
                     label: d["Office Base"].slice(d["Office Base"].indexOf("(") + 1, d["Office Base"].indexOf(")")).toUpperCase()
                 };
                 citiesList.push(newCity);
+                cityIndex = cityIndex + 1
             };
             $scope.models.availableList[d.Week].items.unshift(d);
         });
-        citiesList.sort(function (a, b) { return a.label > b.label; });
+        //JMD 03/05/2016 - Change sort
+        citiesList.sort(function (a, b) { return a.label.localeCompare(b.label); });
         $scope.selBoxCityModel = citiesList.slice(0);
         $scope.selBoxCityData = citiesList;
         $scope.$apply();
     });
-
-    //code before the pause
-    setTimeout(function () {
-        // Generate initial model
-        /*   for (var i = 1; i <= 13; ++i) {
-               $scope.models.lists.A.push({label: "Item A" + i, Grade: "C"});          
-           }
-             console.log("got initial")
-             $scope.$apply();*/
-    }, 2000);
-
-
-    // Model to JSON for demo purpose
-    /*$scope.$watch('models', function(model) {
-            $scope.modelAsJson = angular.toJson(model, true);
-        }, true);*/
 }]);
